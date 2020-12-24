dbc1af82e05312151e673df75a18dd9d708d7c93
Son <hathanhson2706@gmail.com>
2020-12-24 13:20:47 +0100

    Fixed navigation bar

--- website/website/settings.py
+++ website/website/settings.py
@@ -127,8 +127,8 @@ MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-LOGIN_REDIRECT_URL = 'blog-home'
-LOGIN_URL = 'login'
+# LOGIN_REDIRECT_URL = 'blog-home'
+# LOGIN_URL = 'login'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
