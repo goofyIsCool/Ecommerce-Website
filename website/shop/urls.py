af56acabcb75fe7d2402a92ce0e2297461f9b55a
Son <hathanhson2706@gmail.com>
2021-01-16 22:34:08 +0100

    Update

--- website/shop/urls.py
+++ website/shop/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('faq/', views.faq, name='faq'),
     path('contact/', views.contact, name='contact'),
     path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
+    path('order-summary', views.OrderSummaryView.as_view(), name='order-summary'),
     path('add-to-cart/<slug>', views.add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>', views.remove_from_cart, name='remove-from-cart'),
 ]
