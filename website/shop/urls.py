3aee76960f20ce3cb60bae02d6f4fc7bee80519f
Son <hathanhson2706@gmail.com>
2021-01-20 22:56:11 +0100

    Front-End: Fixes

--- website/shop/urls.py
+++ website/shop/urls.py
@@ -10,9 +10,10 @@ urlpatterns = [
     path('faq/', views.faq, name='faq'),
     path('contact/', views.contact, name='contact'),
     path('product/<slug>/', views.ItemDetailView.as_view(), name='product'),
-    path('cart', views.cart, name='cart'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
     # path('add-to-cart/<slug>', views.add_to_cart, name='add-to-cart'),
     # path('remove-from-cart/<slug>', views.remove_from_cart, name='remove-from-cart'),
     path('update_item/', views.updateItem, name='update_item'),
-    path('shipping_update', views.shipping_update, name='shipping_update')
+    path('shipping_update/', views.shipping_update, name='shipping_update')
 ]
--- website/shop/views.py
+++ website/shop/views.py
@@ -44,19 +44,20 @@ def cart(request):
 
 
 def checkout(request):
-    if request.user.is_authenticated:
-        customer = request.user.customer
-        order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        items = order.orderitem_set.all()
-        cartItems = order.get_cart_items
-    else:
-        # Create empty cart for now for non-logged in user
-        items = []
-        order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
-        cartItems = order['get_cart_items']
+    # if request.user.is_authenticated:
+    #     customer = request.user.customer
+    #     order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    #     items = order.orderitem_set.all()
+    #     cartItems = order.get_cart_items
+    # else:
+    #     # Create empty cart for now for non-logged in user
+    #     items = []
+    #     order = {'get_cart_total': 0, 'get_cart_items': 0, 'shipping': False}
+    #     cartItems = order['get_cart_items']
+    #
+    # context = {'items': items, 'order': order, 'cartItems': cartItems}
 
-    context = {'items': items, 'order': order, 'cartItems': cartItems}
-    return render(request, 'store/checkout.html', context)
+    return render(request, 'shop/checkout.html')
 
 
 def faq(request):
