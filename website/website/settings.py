6ce87e679ba392e5293dac31cab6129d54577f67
Son <hathanhson2706@gmail.com>
2021-06-10 14:07:53 +0200

    upgrade

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.17', '192.168.0.102', '192.168.0.103', '192.168.0.101', 'wolkamoda.pl']
+ALLOWED_HOSTS = ['192.168.1.17', '192.168.0.102', '192.168.0.103', '192.168.0.104', 'wolkamoda.pl']
 
 # Application definition
 
